c96e3cf0853ae4915344fabc094c8ba43351dd70
ERIC RICHARD MALAVENDA <ERICMALAVENDA@GATECH.EDU>
2019-03-03 12:44:22 -0500

    map component added to app and rendered in app.component.html. Currently interactive. Removing map's direct query feature. Map will render based on queried notam coords.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchFormComponent } from './search-form/search-form.component';
-import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { MoreInfoComponent } from './more-info/more-info.component';
+import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchFormComponent,
     MoreInfoComponent,
+    MapComponent,
   ],
   imports: [
     NgbModule,
     BrowserModule,
+    AgmCoreModule.forRoot({apiKey: 'AIzaSyDGapNAE5nKEqgzjy3zjzxqRIZRYNPYRlE'}), // <---
     HttpClientModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    GoogleMapsAPIWrapper // <---
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
