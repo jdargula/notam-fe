f4aa8a1b1dfd04d1fb58c9a614851f9aa9c1a535
ERIC RICHARD MALAVENDA <ERICMALAVENDA@GATECH.EDU>
2019-04-14 18:43:24 -0400

    multiple markers now appear on map with correct infoWindow attached to each.

--- src/app/agm-map/agm-map.component.ts
+++ src/app/agm-map/agm-map.component.ts
@@ -1,12 +1,11 @@
 import { Component, NgModule, NgZone, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ViewChild } from '@angular/core';
-import { AgmMap, LatLng, LatLngBoundsLiteral, MapsAPILoader } from '@agm/core';
+import {AgmMap, AgmMarker, LatLng, LatLngBoundsLiteral, MapsAPILoader} from '@agm/core';
 import { GoogleMapsAPIWrapper } from '@agm/core/services';
-import { AgmMarker } from '@agm/core';
 import { AgmCoreModule } from '@agm/core';
 import { SearchFormComponent } from '../search-form/search-form.component';
-import {InfoWindow, Marker} from '@agm/core/services/google-maps-types';
+import {GoogleMap, InfoWindow, MapOptions, Marker} from '@agm/core/services/google-maps-types';
 
 declare var google: any;
 const icao = require('icao');
@@ -26,18 +25,22 @@ const icao = require('icao');
 export class AgmMapComponent implements OnInit {
   private apiRoot: string;
   private displayResponse: boolean;
-  private marker: any;
+  private marker: Marker;
   private airportCoords: Object;
   private latitude: number;
   private longitude: number;
-  private zoom: number;
+  private readonly zoom: number;
+  private eventListener: EventListenerOrEventListenerObject;
   private radius: number;
   private color: string;
   private m: any;
   private airportLatLng: LatLng;
+  private airportLatLngArray: Array<LatLng> = [];
   private airportCode: string;
   private type: string;
-  private markers: Array<any> = [];
+  private infoWindow: InfoWindow;
+  private infoWindowArray: Array<InfoWindow> = [];
+  private markers: Array<Marker> = [];
   private latitude_infoWindow: number;
   private longitude_infoWindow: number;
   /**
@@ -60,13 +63,17 @@ export class AgmMapComponent implements OnInit {
   private top: number;
   private right: number;
   private icaoConversion: string;
-  private coordsArray: Array<object>;
+  private coordsArray: Array<object> = [];
   private coordsArray_lat: object;
   private coordsArray_lng: object;
   private maxWidth: number;
-  private typeOfNotamArrayOnInit: any[];
+  private typeOfNotamArrayOnInit: Array<string> = [];
   private notams: any;
-  private airportCodesArrayOnInit: any[];
+  private airportCodesArrayOnInit: Array<string> = [];
+  private contentString: string;
+  private openInfoWindow: InfoWindow;
+  private previousMarkers: Array<Marker>;
+  private center: LatLng;
   constructor(private API_Loader: MapsAPILoader,
               private zone: NgZone,
               private wrapper: GoogleMapsAPIWrapper,
@@ -76,8 +83,13 @@ export class AgmMapComponent implements OnInit {
     this.zone = zone;
     this.displayResponse = false;
     this.wrapper = wrapper;
+    this.zoom = 4;
+    this.API_Loader.load().then(() => {
+      this.center = new google.maps.LatLng(39.8333333, -98.585522);
+    });
   }
-  @ViewChild(AgmMap) map: AgmMap;
+  @ViewChild(AgmMap) map: GoogleMap;
+  private mapOptions: MapOptions;
 
   ngOnInit() {
     this.displayAllActiveNOTAMs();
@@ -85,72 +97,13 @@ export class AgmMapComponent implements OnInit {
 
   displayAllActiveNOTAMs() {
     this.API_Loader.load().then(() => {
-        this.http.post(this.apiRoot + '/GetAllNotams', 'IATA/ICAO').subscribe(
-          res => {
-            console.log(res);
-            console.log(JSON.stringify(res));
-            this.notams = res;
-            const notamKeyOnInit = [];
-            const airportCodesOnInit = [];
-            const typeOfNotamOnInit = [];
-            let initAirportCode = '';
-            let initNotamType = '';
-            console.log(this.searchForm.notams);
-            /**
-             * ***Note: The fact that the frontend client makes the request by http to post all of the data
-             * for rendering at runtime, storing the data locally in multiple arrays, would not be considered
-             * good practice in data-intensive cases. In an implementation mindful of a user's resources for
-             * storage, old notam data would likely be stored in some separate archival backend that makes
-             * only a fraction of the calls to our app's backend compared to requests made by the frontend).
-             * With this current implementation, the user's device would be responsible for caching
-             * the data and then the app could be set to purge the data when the user log's off,
-             * or refreshes the cache when submitting a query for updated NOTAM data.
-             */
-            this.notams.forEach(function (notam) {
-              notamKeyOnInit.push(notam.col1);
-              initAirportCode = notam.col2;
-              airportCodesOnInit.push(initAirportCode);
-              initNotamType = notam.col3;
-              typeOfNotamOnInit.push(initNotamType);
-            });
-            this.airportCodesArrayOnInit = airportCodesOnInit;
-            console.log(this.airportCodesArrayOnInit);
-            this.typeOfNotamArrayOnInit = typeOfNotamOnInit;
-            console.log(this.typeOfNotamArrayOnInit);
-            for (let index = 0; index < this.airportCodesArrayOnInit.length; index++) {
-              if (this.airportCodesArrayOnInit[index] !== null) {
-                this.airportCode = this.airportCodesArrayOnInit[index];
-                if (this.airportCode === '!FDC') {
-                  this.airportCode = '!IAD';
-                }
-                this.type = this.typeOfNotamArrayOnInit[index];
-                this.icaoConversion = 'K'
-                  + this.airportCode[1]
-                  + this.airportCode[2]
-                  + this.airportCode[3];
-                this.coordsArray = icao[this.icaoConversion];
-                this.coordsArray_lat = this.coordsArray[0];
-                this.coordsArray_lng = this.coordsArray[1];
-                this.airportCoords = {lat: this.coordsArray_lat, lng: this.coordsArray_lng};
-                console.log(this.airportCoords);
-                console.log('index = ' + index);
-                this.m = JSON.parse(JSON.stringify(this.airportCoords));
-                console.log(this.m);
-                this.latitude = parseFloat(this.m.lat);
-                this.longitude = parseFloat(this.m.lng);
-                this.latitude_infoWindow = Math.round(parseFloat(this.m.lat) * 10000) / 10000;
-                this.longitude_infoWindow = Math.round(parseFloat(this.m.lng) * 10000) / 10000;
-                this.airportLatLng = new google.maps.LatLng({lat: this.latitude, lng: this.longitude});
-                this.maxWidth = 500;
-                this.addMarker(this.airportLatLng);
-                this.zoom = 4;
-                this.color = 'red';
-                this.radius = 5000;
-                console.log(this.markers);
-              }
-            }
-            this.setMapOnAll(this.map);
-          });
+      this.http.post(this.apiRoot + '/GetAllNotams', 'IATA/ICAO').subscribe(
+        res => {
+          console.log(res);
+          console.log(JSON.stringify(res));
+          this.notams = res;
+          this.displayNotams(this.notams);
+        });
         this.displayResponse = true;
       }, err => {
         console.error(err);
@@ -158,36 +111,128 @@ export class AgmMapComponent implements OnInit {
     );
   }
 
-  addMarker(location) {
-    this.airportLatLng = location;
-    this.marker = new google.maps.Marker({
-      position: this.airportLatLng,
-      lat: this.airportLatLng.lat(),
-      lng: this.airportLatLng.lng(),
-      map: this.map,
+  displayNotams(notams) {
+    this.notams = notams;
+    const notamKeyOnInit = [];
+    const airportCodesOnInit = [];
+    const typeOfNotamOnInit = [];
+    let initAirportCode = '';
+    let initNotamType = '';
+    console.log(this.searchForm.notams);
+    this.setMapOptions();
+    /**
+     * ***Note: The fact that the frontend client makes the request by http to post all of the data
+     * for rendering at runtime, storing the data locally in multiple arrays, would not be considered
+     * good practice in data-intensive cases. In an implementation mindful of a user's resources for
+     * storage, old notam data would likely be stored in some separate archival backend that makes
+     * only a fraction of the calls to our app's backend compared to requests made by the frontend).
+     * With this current implementation, the user's device would be responsible for caching
+     * the data and then the app could be set to purge the data when the user log's off,
+     * or refreshes the cache when submitting a query for updated NOTAM data.
+     */
+    this.notams.forEach(function (notam) {
+      notamKeyOnInit.push(notam.col1);
+      initAirportCode = notam.col2;
+      airportCodesOnInit.push(initAirportCode);
+      initNotamType = notam.col3;
+      typeOfNotamOnInit.push(initNotamType);
     });
-    this.markers.push(this.marker);
+    this.airportCodesArrayOnInit = airportCodesOnInit;
+    console.log(this.airportCodesArrayOnInit);
+    this.typeOfNotamArrayOnInit = typeOfNotamOnInit;
+    console.log(this.typeOfNotamArrayOnInit);
+    while (this.airportCodesArrayOnInit.length > 0) {
+      this.airportCode = this.airportCodesArrayOnInit.pop();
+      if (this.airportCode === '!FDC') {
+        this.airportCode = '!IAD';
+      }
+      this.type = this.typeOfNotamArrayOnInit.pop();
+      this.icaoConversion = 'K'
+        + this.airportCode[1]
+        + this.airportCode[2]
+        + this.airportCode[3];
+      this.coordsArray = icao[this.icaoConversion];
+      this.coordsArray_lat = this.coordsArray[0];
+      this.coordsArray_lng = this.coordsArray[1];
+      this.airportCoords = {lat: this.coordsArray_lat, lng: this.coordsArray_lng};
+      console.log(this.airportCoords);
+      this.m = JSON.parse(JSON.stringify(this.airportCoords));
+      console.log(this.m);
+      this.latitude = parseFloat(this.m.lat);
+      this.longitude = parseFloat(this.m.lng);
+      this.latitude_infoWindow = Math.round(parseFloat(this.m.lat) * 10000) / 10000;
+      this.longitude_infoWindow = Math.round(parseFloat(this.m.lng) * 10000) / 10000;
+      this.airportLatLng = new google.maps.LatLng({
+        lat: this.latitude,
+        lng: this.longitude
+      });
+      this.maxWidth = 500;
+      this.contentString = '[Type]: ' + this.type + '\n'
+        + '[IATA/ICAO]: ' + this.airportCode + '/' + this.icaoConversion + '\n'
+        + '[Coordinates]: ' + this.airportLatLng.lat() + ', ' + this.airportLatLng.lng();
+      this.infoWindow = new google.maps.InfoWindow({
+        maxWidth: this.maxWidth,
+        content: this.contentString,
+        lat: this.airportLatLng.lat(),
+        lng: this.airportLatLng.lng()
+      });
+      this.airportLatLngArray.push(this.airportLatLng);
+      this.infoWindowArray.push(this.infoWindow);
+    }
+    this.setMarkers(this.map, this.airportLatLngArray, this.infoWindowArray);
   }
 
-  setMapOnAll(map) {
+  setMapOptions() {
+    this.mapOptions = {
+      /*center: The geographic center of the 48 states*/
+      /*source: https://en.wikipedia.org/wiki/Geographic_center_of_the_contiguous_United_States*/
+      center: this.center,
+      zoom: this.zoom,
+      mapTypeId: google.maps.MapTypeId.SATELLITE
+    };
+    this.map = new google.maps.Map(document.getElementById('map'),
+      this.mapOptions);
+  }
+  setMarkers(map, airportLatLngArray, infoWindowArray) {
     this.map = map;
-    for (let i = 0; i < this.markers.length; i++) {
-      this.markers[i].setMap(this.map);
+    this.airportLatLngArray = airportLatLngArray;
+    this.infoWindowArray = infoWindowArray;
+    while (this.airportLatLngArray.length > 0) {
+      console.log('this.airportLatLngArray: ' + this.airportLatLngArray);
+      this.airportLatLng = this.airportLatLngArray.pop();
+      console.log('this.airportLatLng = ' + this.airportLatLng);
+      console.log('this.airportLatLngArray = ' + this.airportLatLngArray);
+      this.infoWindow = this.infoWindowArray.pop();
+      this.marker = new google.maps.Marker({
+        map: this.map, position: this.airportLatLng
+      });
+      this.map.setCenter(this.airportLatLng);
+      this.markers.push(this.marker);
+      console.log(this.markers);
+      this.eventListener = google.maps.event.addListener(this.marker, 'click', (
+        function (marker, currentContent, currentInfoWindow) {
+        return function () {
+          currentInfoWindow.setContent(currentContent);
+          currentInfoWindow.open(map, marker);
+        };
+      })(this.marker, this.infoWindow.getContent(), new google.maps.InfoWindow()));
     }
   }
 
   deleteMarkers() {
     this.clearMarkers();
-    this.markers = [];
   }
 
-  // Removes the markers from the map, but keeps them in the array.
   clearMarkers() {
-    this.setMapOnAll(null);
+    this.resetMarkers(null, null, null);
   }
 
+  resetMarkers(map, airportLatLngArray, infoWindowArray) {
+    this.map = map;
+    this.airportLatLngArray = airportLatLngArray;
+    this.infoWindowArray = infoWindowArray;
+  }
   initialize(m) {
-    this.deleteMarkers();
     this.API_Loader.load().then(() => {
       this.m = m;
       console.log(m);
@@ -196,15 +241,28 @@ export class AgmMapComponent implements OnInit {
       this.latitude_infoWindow = Math.round(parseFloat(this.m.lat) * 10000) / 10000;
       this.longitude_infoWindow = Math.round(parseFloat(this.m.lng) * 10000) / 10000;
       this.airportLatLng = new google.maps.LatLng({lat: this.latitude, lng: this.longitude});
-      this.addMarker(this.airportLatLng);
-      this.maxWidth = 500;
-      this.zoom = 4;
-      this.color = 'red';
-      this.radius = 5000;
+      this.airportLatLngArray.push(this.airportLatLng);
+      this.contentString = '[Type]: ' + this.type + '\n'
+        + '[IATA/ICAO]: ' + this.airportCode + '/' + this.icaoConversion + '\n'
+        + '[Coordinates]: ' + this.airportLatLng.lat() + ', ' + this.airportLatLng.lng();
+      this.infoWindow = new google.maps.InfoWindow({
+        maxWidth: this.maxWidth,
+        content: this.contentString,
+        lat: this.airportLatLng.lat(),
+        lng: this.airportLatLng.lng()
+      });
+      this.airportLatLngArray.push(this.airportLatLng);
+      this.infoWindowArray.push(this.infoWindow);
     });
+    this.setMarkers(this.map, this.airportLatLngArray, this.infoWindowArray);
   }
 
   setAirportCoordinates() {
+    this.deleteMarkers();
+    this.airportLatLngArray = new Array<LatLng>();
+    this.infoWindowArray = new Array<InfoWindow>();
+    this.airportLatLng = new google.maps.LatLng();
+    this.setMapOptions();
     if (this.searchForm.searchForm.value.airport.length === 3) {
       this.searchForm.searchForm.value.airport = '!'
         + this.searchForm.searchForm.value.airport[0]
@@ -218,26 +276,24 @@ export class AgmMapComponent implements OnInit {
     this.searchForm.searchForm.value.airport = this.searchForm.searchForm.value.airport.toUpperCase();
     this.airportCode = this.searchForm.searchForm.value.airport;
     this.type = this.searchForm.searchForm.value.type;
-    // marker colors will be coded according to type.Two additional
-    // types will populate the map when user conducts a query search:
-    // (1) "no notams found," and (2) Type not specified by user.
+    /**
+     * marker colors will be coded according to type.Two additional
+     * types will populate the map when user conducts a query search:
+     * (1) "no notams found," and (2) Type not specified by user.
+     */
     this.icaoConversion = this.icaoConversion.toUpperCase();
-    // Info from http post call below is not being used at this time, due to current formatting of lat and lng values in our db.
-    // Workaround discovered is the "icao" npm imported node package module. Although this limits functionality
-    // of our app to some extent, the icao package works great for our needs at this time.
-    this.http.post(this.apiRoot + '/LongandLatfromCoords', this.searchForm.searchForm.value.airport).subscribe(
+    /**
+     * Info from http post call below is not being used at this time, due to current formatting of lat and lng values in our db.
+     * Workaround discovered is the "icao" npm imported node package module. Although this limits functionality
+     * of our app to some extent, the icao package works great for our needs at this time.
+     */
+    this.http.post(this.apiRoot + '/AirportCodeMultiple', this.searchForm.searchForm.value.airport).subscribe(
        res => {
-        this.displayResponse = true;
-        this.coordsArray = icao[this.icaoConversion];
-        this.coordsArray_lat = this.coordsArray[0];
-        this.coordsArray_lng = this.coordsArray[1];
-        this.airportCoords = {lat: this.coordsArray_lat, lng: this.coordsArray_lng};
-        console.log(this.airportCoords);
-        res = this.airportCoords;
-        console.log(this.airportCoords);
-        this.m = JSON.parse(JSON.stringify(res));
-        console.log(this.m);
-        this.initialize(this.m);
+         console.log(res);
+         console.log(JSON.stringify(res));
+         this.notams = res;
+         this.displayNotams(this.notams);
+         this.displayResponse = true;
       }, err => {
          console.error(err);
       }
