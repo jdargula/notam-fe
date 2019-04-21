71c7db84a2b5f70072d672ea682a2eed58924330
Jay <jaydargula@gmail.com>
2019-04-21 14:56:05 -0400

    table works but lat/long formatting issue.

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,183 @@
+/* tslint:disable:prefer-const */
+// map/map.component.ts
+
+import { Component, ViewChild, NgZone, OnInit } from '@angular/core';
+import { MapsAPILoader, AgmMap } from '@agm/core';
+import { GoogleMapsAPIWrapper } from '@agm/core/services';
+import {HttpClient} from '@angular/common/http';
+
+declare var google: any;
+
+interface Marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+}
+
+interface Location {
+  lat: number;
+  lng: number;
+  viewport?: Object;
+  zoom: number;
+  address_level_1?: string;
+  address_level_2?: string;
+  address_country?: string;
+  address_zip?: string;
+  address_state?: string;
+  marker?: Marker;
+}
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+
+export class MapComponent implements OnInit {
+  private apiRoot: string;
+  private displayResponse: boolean;
+  private displayMoreDetails: boolean;
+  private notams: Object;
+  constructor(public mapsApiLoader: MapsAPILoader,
+              private zone: NgZone,
+              private wrapper: GoogleMapsAPIWrapper,
+              private http: HttpClient) {
+    this.apiRoot = 'http://localhost:8080';
+    this.displayResponse = false;
+    this.displayMoreDetails = false;
+    this.mapsApiLoader = mapsApiLoader;
+    this.zone = zone;
+    this.wrapper = wrapper;
+    this.mapsApiLoader.load().then(() => {
+      this.geocoder = new google.maps.Geocoder();
+    });
+  }
+  geocoder: any;
+  public location: Location = {
+    lat: 0,
+    lng: 0,
+    marker: {
+      lat: 0,
+      lng: 0,
+      draggable: true
+    },
+    zoom: 5
+  };
+  @ViewChild(AgmMap) map: AgmMap;
+
+  ngOnInit() {
+    this.location.marker.draggable = true;
+  }
+
+  showMap() {
+    this.http.post(this.apiRoot + '/LongandLatfromCoords', 'ATL').subscribe(
+      res => {
+        console.log('res');
+        console.log(JSON.stringify(res));
+        this.notams = res;
+        this.location.lat = this.notams[0];
+        this.location.lng = this.notams[1];
+        this.displayResponse = true;
+      }, err => {
+        console.error(err);
+      }
+    );
+  }
+
+  findLocation(address) {
+    if (!this.geocoder) { this.geocoder = new google.maps.Geocoder(); }
+    this.geocoder.geocode({
+      'address': address
+    }, (results, status) => {
+      console.log(results);
+      if (status === google.maps.GeocoderStatus.OK) {
+        this.updateOnMap(this.location);
+        for (let i = 0; i < results[0].address_components.length; i++) {
+          let types = results[0].address_components[i].types;
+
+          if (types.indexOf('locality') !== -1) {
+            this.location.address_level_2 = results[0].address_components[i].long_name;
+          }
+          if (types.indexOf('country') !== -1) {
+            this.location.address_country = results[0].address_components[i].long_name;
+          }
+          if (types.indexOf('postal_code') !== -1) {
+            this.location.address_zip = results[0].address_components[i].long_name;
+          }
+          if (types.indexOf('administrative_area_level_1') !== -1) {
+            this.location.address_state = results[0].address_components[i].long_name;
+          }
+        }
+
+        if (results[0].geometry.location) {
+          this.location.lat = results[0].geometry.location.lat();
+          this.location.lng = results[0].geometry.location.lng();
+          this.location.marker.lat = results[0].geometry.location.lat();
+          this.location.marker.lng = results[0].geometry.location.lng();
+          this.location.marker.draggable = true;
+          this.location.viewport = results[0].geometry.viewport;
+        }
+
+        this.map.triggerResize();
+      } else {
+        alert('Sorry, this search produced no results.');
+      }
+    });
+  }
+
+  notamLocationOnMap(m: any) { 
+    this.location.marker.lat = m.coords.lat;
+    this.location.marker.lng = m.coords.lng;
+    this.findAddressByCoordinates();
+  }
+
+  findAddressByCoordinates() {
+    this.geocoder.geocode({
+      'location': {
+        lat: this.location.marker.lat,
+        lng: this.location.marker.lng,
+      }
+    }, (results, status) => {
+      this.decomposeAddressComponents(results);
+    });
+  }
+
+  decomposeAddressComponents(addressArray) {
+    if (addressArray.length === 0) { return false; }
+    let address = addressArray[0].address_components;
+
+    for (let element of address) {
+      if (element.length === 0 && !element['types']) { continue; }
+
+      if (element['types'].indexOf('street_number') > -1) {
+        this.location.address_level_1 = element['long_name'];
+        continue;
+      }
+      if (element['types'].indexOf('route') > -1) {
+        this.location.address_level_1 += ', ' + element['long_name'];
+        continue;
+      }
+      if (element['types'].indexOf('locality') > -1) {
+        this.location.address_level_2 = element['long_name'];
+        continue;
+      }
+      if (element['types'].indexOf('administrative_area_level_1') > -1) {
+        this.location.address_state = element['long_name'];
+        continue;
+      }
+      if (element['types'].indexOf('country') > -1) {
+        this.location.address_country = element['long_name'];
+        continue;
+      }
+      if (element['types'].indexOf('postal_code') > -1) {
+        this.location.address_zip = element['long_name'];
+
+      }
+    }
+  }
+
+  updateOnMap(location) {
+    this.notamLocationOnMap(location);
+  }
+}
